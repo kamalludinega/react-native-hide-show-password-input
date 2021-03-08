3464a8aab186ba608b1a88e0ef4fec6dc9427ceb
Ega Kamalludin <kamalludinega@gmail.com>
2021-03-08 17:24:55 +0700

    fix use native driver issue

--- src/component/passwordInput.js
+++ src/component/passwordInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from "prop-types";
-import { TextField } from 'react-native-material-textfield';
+import { TextField } from 'rn-material-ui-textfield';
 
 class PasswordInputText extends React.Component {
 
